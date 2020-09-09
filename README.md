5d739f2a6d7d0cefe23c34041b43e605a04abd0c
miszad89 <miszad89@gmail.com>
2020-09-09 18:01:39 +0200

    Restyle desktop menu, repair scrolling function and complete README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# czterylapy
+<h1 align="center">Cztery Łapy</h1>
+<p>This is a simple project of vet clinic website.</p>
+<p>Original website address: http://www.gabinet4lapy.pl/</p>
+<p>This is non-commercial project</p>
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 const menuItems = menu.querySelectorAll(".menu__item");
-const aboutTop = document.querySelector(".about").offsetTop;
-const locationsTop = document.querySelector(".locations").offsetTop;
-const offerTop = document.querySelector(".offer").offsetTop;
+const logo = document.querySelector(".logo");
 
 const handleClickHamburger = () => {
   hamburger.classList.toggle("hamburger--active");
@@ -11,26 +9,30 @@ const handleClickHamburger = () => {
 };
 
 const scrollPage = (e) => {
-  const resetScroll = () => window.scrollTo({ top: 0, behavior: "smooth" });
+  e.preventDefault();
+  const aboutTop = document.querySelector(".about").offsetTop;
+  const locationsTop = document.querySelector(".locations").offsetTop;
+  const offerTop = document.querySelector(".offer").offsetTop;
 
   switch (e.target.innerText) {
     case "O NAS":
-      resetScroll();
       window.scrollTo({ top: `${aboutTop}`, behavior: "smooth" });
       break;
     case "GABINETY":
-      resetScroll();
       window.scrollTo({ top: `${locationsTop}`, behavior: "smooth" });
       break;
     case "ZAKRES USŁUG":
-      resetScroll();
       window.scrollTo({ top: `${offerTop}`, behavior: "smooth" });
+      break;
+    case "":
+      window.scrollTo({ top: 0, behavior: "smooth" });
       break;
     default:
       console.log(`There is no such section as ${e.target.innerText}`);
   }
-  handleClickHamburger();
+  menu.classList.remove("menu--active");
 };
 
 hamburger.addEventListener("click", handleClickHamburger);
+logo.addEventListener("click", scrollPage);
 menuItems.forEach((item) => item.addEventListener("click", scrollPage));
